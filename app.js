f3b3006e5be33d05f9213854f4e8b9ff7aed9a06
chaniwolfson <gutman.chani@gmail.com>
2026-09-08 11:30:59 +0300

    feat: display configmap variables and APP_VERSION in /my-app and /version

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ const APP_VERSION = process.env.APP_VERSION || packageJson.version;
 const APP_ENV = process.env.APP_ENV || 'development';
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
 
-console.log(`App running in ${APP_ENV} mode with log level ${LOG_LEVEL}`);
-
 // Enable the collection of default metrics
 promClient.collectDefaultMetrics({ register });
 
@@ -31,9 +29,15 @@ app.get('/version', (req, res) => {
 });
 
 // Define routes
+// עדכון הנתיב my-app
 app.get('/my-app', (req, res) => {
     helloWorldCounter.inc();
-    res.send('Hello, World!');
+    res.json({
+        message: 'Hello, World!',
+        environment: APP_ENV,
+        logLevel: LOG_LEVEL,
+        version: APP_VERSION
+    });
 });
 
 app.get('/about', (req, res) => {
